abd05568ead5c69475f30c1324334310c36810a2
basonzzzz1 <132861064+basonzzzz1@users.noreply.github.com>
2023-09-08 17:27:35 +0700

    update comment controller 17:27 08/09

--- src/main/java/com/be/controller/CommentController.java
+++ src/main/java/com/be/controller/CommentController.java
@@ -1,7 +1,11 @@
 package com.be.controller;
 
+import com.be.model.Account;
 import com.be.model.Comment;
+import com.be.model.Product;
+import com.be.service.impl.AccountServiceImpl;
 import com.be.service.impl.CommentServiceImpl;
+import com.be.service.impl.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +13,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
     @Autowired
     CommentServiceImpl commentService;
+    @Autowired
+    ProductServiceImpl productService;
+    @Autowired
+    AccountServiceImpl accountService;
 
     @GetMapping
     public String viewComments(Model model) {
@@ -22,11 +31,18 @@ public class CommentController {
         model.addAttribute("comments", comments);
         return "comments";
     }
-    @PostMapping("/add")
+    @PostMapping("/add/{productId}/{id}")
     @ResponseBody
-    public String addComment(@RequestBody Comment comment) {
-        comment.setCreatedAt(LocalDateTime.now());
-        commentService.save(comment);
-        return "Comment added successfully!";
+    public String addComment(@PathVariable int productId, @RequestBody Comment comment, @PathVariable int id) {
+        Optional<Product> product = Optional.ofNullable(productService.findById(productId));
+        if (product.isPresent()) {
+            comment.setProduct(product.get());
+            comment.setCreatedAt(LocalDateTime.now());
+            comment.setAccount(accountService.findById(id));
+            commentService.save(comment);
+            return "Comment added successfully!";
+        } else {
+            return "Product not found!";
+        }
     }
 }
